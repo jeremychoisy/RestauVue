74492b53b148d0773578b76d54b3bbe0ad819975
Da SIlva Goncalves andré <andredasilvagoncalves@hotmail.com>
2019-12-10 15:17:33 +0100

    add  table element

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { Button, } from 'element-ui';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.component(Button.name, Button);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+}).$mount('#app');
+
---
+++ src/page/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> RestauVue by Jeremy CHOISY and André DA SILVA GONCALVES</h1>
+        <Table/>
+    </div>
+</template>
+
+<script>
+    import Table from '../components/Table'
+
+    export default {
+        name: "mainPage",
+        components: {
+            Table
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
