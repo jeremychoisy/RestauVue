c2cda1705168b5f68e14b628d2fab8625e2aa312
Jérémy Choisy <choisy.jeremy@gmail.com>
2019-12-15 14:28:10 +0100

    Vue2-google-maps module setup

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
-
 import ElementUI from 'element-ui';
-import { Button, } from 'element-ui';
+
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false;
+
 Vue.use(ElementUI);
-Vue.component(Button.name, Button);
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyCyZlvP6hBkWH7AKttIegLBdXWSmxO06Ag',
+        libraries: 'places'
+    }
+});
 
 new Vue({
     el: '#app',
