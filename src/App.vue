a369a1749239aa6b31096cdc2eb6832a436d427c
Jérémy Choisy <choisy.jeremy@gmail.com>
2020-01-11 19:17:02 +0100

    add drawer styling

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,42 @@
 </template>
 
 <script>
-
 export default {
   name: 'app',
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+  .el-drawer__body {
+    height: 100%;
+    box-sizing: border-box;
+    overflow-y: auto;
+  }
 </style>
